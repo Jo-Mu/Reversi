a51417cfe1c851b99854a679eaf20f382e93253e
Jonathan Muniz <33111715+Jo-Mu@users.noreply.github.com>
2018-06-01 15:52:05 -0400

    Added PositionState class with State enumerator

---
+++ PositionState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversi
+{
+    class PositionState
+    {
+        public enum State
+        {
+            Empty = 0,
+            Cross = 1,
+            Circle = 2
+        };
+    }
+}

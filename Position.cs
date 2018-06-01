92d82b8fa758d8ac84c889a9437a6ac638646d8e
Jonathan Muniz <33111715+Jo-Mu@users.noreply.github.com>
2018-06-01 19:40:42 -0400

    Added Postition struct for choosing locations on board

---
+++ Position.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversi
+{
+    struct Position
+    {
+        public int x;
+        public int y;
+
+        public Position(int in_x, int in_y)
+        {
+            x = in_x;
+            y = in_y;
+        }
+
+        public static Position operator +(Position pos1, Position pos2)
+        {
+            return new Position(pos1.x + pos2.x, pos1.y + pos2.y);
+        }
+
+        public static Position operator -(Position pos1, Position pos2)
+        {
+            return new Position(pos1.x - pos2.x, pos1.y - pos2.y);
+        }
+    }
+}

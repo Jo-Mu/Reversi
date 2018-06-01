547f3737ea7949d505b637bab2670fd51c0fd4ac
Jonathan Muniz <33111715+Jo-Mu@users.noreply.github.com>
2018-06-01 17:38:40 -0400

    Added Board class for game board and created method for drawing the board.

---
+++ Board.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Reversi.PositionState;
+
+namespace Reversi
+{
+    class Board
+    {
+        public State[,] _board;
+        private readonly int _sideDimensions;
+
+        public Board(int sideDimensions)
+        {
+            if(sideDimensions < 4)
+            {
+                throw new InvalidBoardSizeException("Dimensions are too small");
+            }
+
+            if(sideDimensions % 2 != 0)
+            {
+                throw new InvalidBoardSizeException("Dimensions must be even");
+            }
+
+            _board = new State[sideDimensions, sideDimensions];
+            _sideDimensions = sideDimensions;
+
+            for(int x = (sideDimensions / 2) - 1; x <= sideDimensions / 2; x++)
+            {
+                for (int y = (sideDimensions / 2) - 1; y <= sideDimensions / 2; y++)
+                {
+                    if((x + y) % 2 == 0)
+                    {
+                        _board[y, x] = State.Circle;
+                    }
+                    else
+                    {
+                        _board[y, x] = State.Cross;
+                    }
+                }
+            }
+        }
+
+        public void DrawBoard()
+        {
+            for (int y = 0; y <= _sideDimensions; y++)
+            {
+                for (int x = 0; x <= _sideDimensions; x++)
+                {
+                    if (y == 0)
+                    {
+                        if (x == 0)
+                        {
+                            Console.Write("   " + x);
+                        }
+                        else if (x < _sideDimensions)
+                        {
+                            Console.Write("  " + x);
+                        }
+                    }
+                    else
+                    {
+                        if (x == 0)
+                        {
+                            Console.Write((y - 1) + " ");
+                        }
+                        else
+                        {
+                            switch (_board[y - 1, x - 1])
+                            {
+                                case State.Empty:
+                                    {
+                                        Console.Write("[ ]");
+                                        break;
+                                    }
+                                case State.Cross:
+                                    {
+                                        Console.Write("[X]");
+                                        break;
+                                    }
+                                case State.Circle:
+                                    {
+                                        Console.Write("[O]");
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        break;
+                                    }
+                            }
+                        }
+                    }
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
--- Game.cs
+++ Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Reversi.PositionState;
 
 namespace Reversi
 {
@@ -10,6 +11,12 @@ namespace Reversi
     {
         static void Main(string[] args)
         {
+            State player1 = State.Cross;
+            State player2 = State.Circle;
+            int boardSideDimensions = 8;
+            Board board = new Board(boardSideDimensions);
+
+            board.DrawBoard();
         }
     }
 }

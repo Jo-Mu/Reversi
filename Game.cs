9ac748e1ea26266ec5a2343582341b7ea780a39e
Jonathan Muniz <33111715+Jo-Mu@users.noreply.github.com>
2018-06-04 14:25:05 -0400

    Added PlayerTurn method and Play method to GameMatch class. Now the game can be played with each player taking a turn.

--- Game.cs
+++ Game.cs
@@ -9,113 +9,163 @@ namespace Reversi
 {
     class Game
     {
+
         static void Main(string[] args)
         {
-            State player1 = State.Cross;
-            State player2 = State.Circle;
             int boardSideDimensions = 8;
             Board board = new Board(boardSideDimensions);
 
             board.DrawBoard();
+            GameMatch match = new GameMatch(board);
+            match.Play();
+        }
 
-            //All below is testing piece placement and input validation
-            const int cursorOffsetLeft = 3;
-            const int cursorOffsetTop = 1;
-            int cLeft = cursorOffsetLeft;
-            int cTop = cursorOffsetTop;
-            bool enterPressed = false;
-            Position selectedPos = new Position((cLeft / cursorOffsetLeft) - 1, (cTop / cursorOffsetTop) - 1);
-            string turnStatus = "Player 1's turn";
-            Console.WriteLine(turnStatus);
+        void PlayerTurn(ref Board board, State playerState)
+        {
+            bool turnIsOver = false;
+            string initialTurnStatus;
+            string turnStatus;
+            string playerSymbol;
+            string enemySymbol;
 
-            do
+            if(playerState == State.Cross)
             {
-                while (!Console.KeyAvailable)
-                {
-                    Console.SetCursorPosition(cLeft, cTop);
-                    Console.Write("X");
-
-                    if(board.GetStateAt(selectedPos) == State.Circle)
-                    {
-                        Console.SetCursorPosition(cLeft, cTop);
-                        Console.Write("O");
-                    }
-                }
-
-                switch (Console.ReadKey().Key)
-                {
-                    case ConsoleKey.UpArrow:
-                        {
-                            cTop -= cursorOffsetTop;
-                            
-                            if(cTop <= 0)
-                            {
-                                cTop += cursorOffsetTop;
-                            }
-
-                            break;
-                        }
-                    case ConsoleKey.DownArrow:
-                        {
-                            cTop += cursorOffsetTop;
-
-                            if(cTop > cursorOffsetTop * board.SideDimensions)
-                            {
-                                cTop -= cursorOffsetTop;
-                            }
-
-                            break;
-                        }
-                    case ConsoleKey.LeftArrow:
-                        {
-                            cLeft -= cursorOffsetLeft;
-
-                            if (cLeft <= 0)
-                            {
-                                cLeft += cursorOffsetLeft;
-                            }
-
-                            break;
-                        }
-                    case ConsoleKey.RightArrow:
-                        {
-                            cLeft += cursorOffsetLeft;
-
-                            if (cLeft > cursorOffsetLeft * board.SideDimensions)
-                            {
-                                cLeft -= cursorOffsetLeft;
-                            }
-
-                            break;
-                        }
-                    case ConsoleKey.Enter:
-                        {
-                            enterPressed = true;
-                            break;
-                        }
-                    default:
-                        {
-                            break;
-                        }
-                }
-
-                selectedPos = new Position((cLeft / cursorOffsetLeft) - 1, (cTop / cursorOffsetTop) - 1);
-                Console.SetCursorPosition(0, 0);
-                board.DrawBoard();
-                Console.WriteLine(turnStatus);
-            }
-            while (!enterPressed);
-
-            if (board.AttemptPlacePieceAt(selectedPos, player1))
-            {
-                Console.Clear();
-                board.DrawBoard();
+                initialTurnStatus = "\nPlayer 1's turn:\nPress 'Enter' or 'Space' to place piece.\nIf unable to make a move press 'S' to skip turn.";
+                turnStatus = initialTurnStatus;
+                playerSymbol = "X";
+                enemySymbol = "O";
             }
             else
             {
+                initialTurnStatus = "\nPlayer 2's turn:\nPress 'Enter' or 'Space' to place piece.\nIf unable to make a move press 'S' to skip turn.";
+                turnStatus = initialTurnStatus;
+                playerSymbol = "O";
+                enemySymbol = "X";
+            }
+
+            while (!turnIsOver)
+            {
                 Console.Clear();
                 board.DrawBoard();
-                Console.WriteLine("Can't make move!");
+                Console.WriteLine(turnStatus);
+
+                const int cursorOffsetLeft = 3;
+                const int cursorOffsetTop = 1;
+                int cLeft = cursorOffsetLeft;
+                int cTop = cursorOffsetTop;
+                bool piecePlaced = false;
+                bool skipPressed = false;
+                Position selectedPos = new Position((cLeft / cursorOffsetLeft) - 1, (cTop / cursorOffsetTop) - 1);
+
+                do
+                {
+                    while (!Console.KeyAvailable)
+                    {
+                        Console.SetCursorPosition(cLeft, cTop);
+                        Console.Write(playerSymbol);
+
+                        if (board.GetStateAt(selectedPos) == State.Circle)
+                        {
+                            Console.SetCursorPosition(cLeft, cTop);
+                            Console.Write(enemySymbol);
+                        }
+                    }
+
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            {
+                                cTop -= cursorOffsetTop;
+
+                                if (cTop <= 0)
+                                {
+                                    cTop += cursorOffsetTop;
+                                }
+
+                                break;
+                            }
+                        case ConsoleKey.DownArrow:
+                            {
+                                cTop += cursorOffsetTop;
+
+                                if (cTop > cursorOffsetTop * board.SideDimensions)
+                                {
+                                    cTop -= cursorOffsetTop;
+                                }
+
+                                break;
+                            }
+                        case ConsoleKey.LeftArrow:
+                            {
+                                cLeft -= cursorOffsetLeft;
+
+                                if (cLeft <= 0)
+                                {
+                                    cLeft += cursorOffsetLeft;
+                                }
+
+                                break;
+                            }
+                        case ConsoleKey.RightArrow:
+                            {
+                                cLeft += cursorOffsetLeft;
+
+                                if (cLeft > cursorOffsetLeft * board.SideDimensions)
+                                {
+                                    cLeft -= cursorOffsetLeft;
+                                }
+
+                                break;
+                            }
+                        case ConsoleKey.Enter:
+                        case ConsoleKey.Spacebar:
+                            {
+                                piecePlaced = true;
+                                break;
+                            }
+                        case ConsoleKey.S:
+                            {
+                                skipPressed = true;
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+
+                    selectedPos = new Position((cLeft / cursorOffsetLeft) - 1, (cTop / cursorOffsetTop) - 1);
+                    Console.SetCursorPosition(0, 0);
+                    board.DrawBoard();
+                    Console.WriteLine(turnStatus);
+                }
+                while (!piecePlaced && !skipPressed);
+
+                if (skipPressed)
+                {
+                    break;
+                }
+                else if (!board.IsEmptyAt(selectedPos))
+                {
+                    turnStatus = "\nSpace is already occupied!\n" + initialTurnStatus;
+                    continue;
+                }
+                else if(!board.IsAdjacentToEnemyPiece(selectedPos, playerState))
+                {
+                    turnStatus = "\nPiece is not adjacent to any opponent pieces!\n" + initialTurnStatus;
+                    continue;
+                }
+                else if(board.AttemptPlacePieceAt(selectedPos, playerState))
+                {
+                    turnIsOver = true;
+                    Console.Clear();
+                    board.DrawBoard();
+                }
+                else
+                {
+                    turnStatus = "\nCan not capture any opponent pieces from this position!\n" + initialTurnStatus;
+                    continue;
+                }
             }
         }
     }
--- GameMatch.cs
+++ GameMatch.cs
@@ -17,5 +17,171 @@ namespace Reversi
         {
             _board = board;
         }
+
+        private void PlayerTurn(State playerState)
+        {
+            bool turnIsOver = false;
+            string initialTurnStatus;
+            string turnStatus;
+            string playerSymbol;
+            string enemySymbol;
+
+            if (playerState == State.Cross)
+            {
+                initialTurnStatus = "\nPlayer 1's turn:\nPress 'Enter' or 'Space' to place piece.\nIf unable to make a move press 'S' to skip turn.";
+                turnStatus = initialTurnStatus;
+                playerSymbol = "X";
+                enemySymbol = "O";
+            }
+            else
+            {
+                initialTurnStatus = "\nPlayer 2's turn:\nPress 'Enter' or 'Space' to place piece.\nIf unable to make a move press 'S' to skip turn.";
+                turnStatus = initialTurnStatus;
+                playerSymbol = "O";
+                enemySymbol = "X";
+            }
+
+            while (!turnIsOver)
+            {
+                Console.Clear();
+                _board.DrawBoard();
+                Console.WriteLine(turnStatus);
+
+                const int cursorOffsetLeft = 3;
+                const int cursorOffsetTop = 1;
+                int cLeft = cursorOffsetLeft;
+                int cTop = cursorOffsetTop;
+                bool piecePlaced = false;
+                bool skipPressed = false;
+                Position selectedPos = new Position((cLeft / cursorOffsetLeft) - 1, (cTop / cursorOffsetTop) - 1);
+
+                do
+                {
+                    while (!Console.KeyAvailable)
+                    {
+                        Console.SetCursorPosition(cLeft, cTop);
+                        Console.Write(playerSymbol);
+
+                        if (_board.GetStateAt(selectedPos) == State.Circle)
+                        {
+                            Console.SetCursorPosition(cLeft, cTop);
+                            Console.Write(enemySymbol);
+                        }
+                    }
+
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            {
+                                cTop -= cursorOffsetTop;
+
+                                if (cTop <= 0)
+                                {
+                                    cTop += cursorOffsetTop;
+                                }
+
+                                break;
+                            }
+                        case ConsoleKey.DownArrow:
+                            {
+                                cTop += cursorOffsetTop;
+
+                                if (cTop > cursorOffsetTop * _board.SideDimensions)
+                                {
+                                    cTop -= cursorOffsetTop;
+                                }
+
+                                break;
+                            }
+                        case ConsoleKey.LeftArrow:
+                            {
+                                cLeft -= cursorOffsetLeft;
+
+                                if (cLeft <= 0)
+                                {
+                                    cLeft += cursorOffsetLeft;
+                                }
+
+                                break;
+                            }
+                        case ConsoleKey.RightArrow:
+                            {
+                                cLeft += cursorOffsetLeft;
+
+                                if (cLeft > cursorOffsetLeft * _board.SideDimensions)
+                                {
+                                    cLeft -= cursorOffsetLeft;
+                                }
+
+                                break;
+                            }
+                        case ConsoleKey.Enter:
+                        case ConsoleKey.Spacebar:
+                            {
+                                piecePlaced = true;
+                                break;
+                            }
+                        case ConsoleKey.S:
+                            {
+                                skipPressed = true;
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+
+                    selectedPos = new Position((cLeft / cursorOffsetLeft) - 1, (cTop / cursorOffsetTop) - 1);
+                    Console.SetCursorPosition(0, 0);
+                    _board.DrawBoard();
+                    Console.WriteLine(turnStatus);
+                }
+                while (!piecePlaced && !skipPressed);
+
+                if (skipPressed)
+                {
+                    break;
+                }
+                else if (!_board.IsEmptyAt(selectedPos))
+                {
+                    turnStatus = "\nSpace is already occupied!\n" + initialTurnStatus;
+                    continue;
+                }
+                else if (!_board.IsAdjacentToEnemyPiece(selectedPos, playerState))
+                {
+                    turnStatus = "\nPiece is not adjacent to any opponent pieces!\n" + initialTurnStatus;
+                    continue;
+                }
+                else if (_board.AttemptPlacePieceAt(selectedPos, playerState))
+                {
+                    turnIsOver = true;
+                    Console.Clear();
+                    _board.DrawBoard();
+                }
+                else
+                {
+                    turnStatus = "\nCan not capture any opponent pieces from this position!\n" + initialTurnStatus;
+                    continue;
+                }
+            }
+        }
+
+        public State Play()
+        {
+            while (!_board.IsGameOver())
+            {
+                PlayerTurn(_player1);
+
+                if (_board.IsGameOver())
+                {
+                    break;
+                }
+
+                PlayerTurn(_player2);
+            }
+
+            return _board.TallyWinner();
+        }
     }
 }

569bdb21ca031e350905d1fa1fd32747be5366f7
Jonathan Muniz <33111715+Jo-Mu@users.noreply.github.com>
2018-06-04 13:37:30 -0400

    Added GameMatch class for playing each round.

---
+++ GameMatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Reversi.SpaceState;
+
+namespace Reversi
+{
+    class GameMatch
+    {
+        private const State _player1 = State.Cross;
+        private const State _player2 = State.Circle;
+        private Board _board;
+
+        public GameMatch(Board board)
+        {
+            _board = board;
+        }
+    }
+}
